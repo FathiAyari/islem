437f509beda235ea9f0b65d2fcf5252ec26fbb7f
FathiAyari <fathi3ayari333@gmail.com>
2022-04-22 00:05:31 +0200

    willpopscop alert

--- lib/SignIn/sign_in.dart
+++ lib/SignIn/sign_in.dart
@@ -1,5 +1,8 @@
+import 'dart:io';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
 import 'package:pfe_islem/SignIn/testWidget.dart';
 
 class SignIn extends StatefulWidget {
@@ -32,14 +35,67 @@ class _SignInState extends State<SignIn> {
   ];
   List pages = [testWidget(), Text("Clients"), Text("Settingd")];
   int currentItem = 0;
+
+  Widget positiveCall() {
+    return Container(
+      decoration: BoxDecoration(
+          color: Colors.blueAccent, borderRadius: BorderRadius.circular(20)),
+      child: TextButton(
+          onPressed: () {
+            exit(0);
+          },
+          child: Text(
+            "oui",
+            style: TextStyle(color: Colors.white),
+          )),
+    );
+  }
+
+  Widget negativeCall() {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(20),
+        border: Border.all(color: Colors.blueAccent),
+      ),
+      child: TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: const Text(
+            "non",
+            style: TextStyle(color: Colors.blueAccent),
+          )),
+    );
+  }
+
+  Future<bool> avoidReturnButton() async {
+    showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            title: Column(
+              children: [
+                Lottie.asset("assets/images/error.json", height: 50),
+                Text("Vous etez sur de sortir ?"),
+              ],
+            ),
+            actions: [positiveCall(), negativeCall()],
+          );
+        });
+    return true;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        body: Center(
-          child: pages[currentItem],
+    return WillPopScope(
+      onWillPop: avoidReturnButton,
+      child: SafeArea(
+        child: Scaffold(
+          body: Center(
+            child: pages[currentItem],
+          ),
+          bottomNavigationBar: bottomnavbar(),
         ),
-        bottomNavigationBar: bottomnavbar(),
       ),
     );
   }
--- lib/SignIn/testWidget.dart
+++ lib/SignIn/testWidget.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
 
 class testWidget extends StatelessWidget {
   const testWidget({
@@ -7,6 +11,94 @@ class testWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Text("Home");
+    return Container(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text("Home"),
+          ElevatedButton(
+              onPressed: () {
+                showDialog(
+                    context: context,
+                    builder: (context) {
+                      return AlertDialog(
+                        title: Row(
+                          children: [
+                            Text("Type smthng"),
+                            Transform.translate(
+                              offset: Offset(80, -20),
+                              child: IconButton(
+                                  onPressed: () {
+                                    Navigator.pop(context);
+                                  },
+                                  icon: Icon(Icons.close)),
+                            )
+                          ],
+                        ),
+                        content: Container(
+                          height: 150,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceAround,
+                            children: [
+                              TextFormField(
+                                decoration: InputDecoration(
+                                    border: OutlineInputBorder(
+                                        borderRadius:
+                                            BorderRadius.circular(20)),
+                                    label: Text("type smtng")),
+                              ),
+                              Row(
+                                children: [
+                                  ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                          shape: RoundedRectangleBorder(
+                                              borderRadius:
+                                                  BorderRadius.circular(15)),
+                                          primary: Colors.red),
+                                      onPressed: () {
+                                        Fluttertoast.showToast(
+                                            msg: "This Fathi here :) ",
+                                            toastLength: Toast.LENGTH_SHORT,
+                                            gravity: ToastGravity.BOTTOM,
+                                            timeInSecForIosWeb: 1,
+                                            backgroundColor: Colors.grey,
+                                            textColor: Colors.white,
+                                            fontSize: 16.0);
+                                      },
+                                      child: Text("Cancel")),
+                                  SizedBox(
+                                    width: 20,
+                                  ),
+                                  ElevatedButton(
+                                    onPressed: () {
+                                      Get.snackbar(
+                                        "Pfe Islem",
+                                        "Snack Bar flutter",
+                                        icon: Icon(Icons.person,
+                                            color: Colors.white),
+                                        snackPosition: SnackPosition.BOTTOM,
+                                      );
+                                    },
+                                    child: Text("Confirm"),
+                                    style: ElevatedButton.styleFrom(
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(15)),
+                                        primary: Colors.green),
+                                  ),
+                                ],
+                                mainAxisAlignment: MainAxisAlignment.center,
+                              )
+                            ],
+                          ),
+                        ),
+                      );
+                    });
+              },
+              child: Text("Show alert"))
+        ],
+      ),
+    );
   }
 }
